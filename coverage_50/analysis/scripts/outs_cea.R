221f6267181c915b9b8f02ae6c399fb811c4fdb8
ElenaVG-15 <160852548+ElenaVG-15@users.noreply.github.com>
2026-03-30 10:11:21 +0100

    Add files via upload

--- coverage_50/analysis/scripts/outs_cea.R
+++ coverage_50/analysis/scripts/outs_cea.R
@@ -315,6 +315,7 @@ for (i in 1:length(files)) {
     filter(year >= 2026) %>% 
     filter((str_detect(TB, 'ST') & str_detect(TB, 'count'))) %>% 
     filter(!(str_detect(TB, 'succ'))) %>% 
+    filter(!(str_detect(TB, 'adult'))) %>%  
     filter(!(str_detect(RISK, 'dead') | str_detect(RISK, 'count'))) %>% 
     filter(!(str_detect(VXa, 'dead') | str_detect(VXa, 'count'))) %>% 
     filter((age_from == 0 & age_thru == 99)) %>% 
@@ -355,6 +356,7 @@ for (i in 1:length(files)) {
       filter(year >= 2026) %>% 
       filter((str_detect(TB, 'RT') & str_detect(TB, 'count'))) %>% 
       filter(!(str_detect(TB, 'succ'))) %>% 
+      filter(!(str_detect(TB, 'adult'))) %>%  
       filter(!(str_detect(RISK, 'dead') | str_detect(RISK, 'count'))) %>%
       filter(!(str_detect(VXa, 'dead') | str_detect(VXa, 'count'))) %>% 
       filter((age_from == 0 & age_thru == 99)) %>% 
@@ -661,7 +663,7 @@ for (i in 1:length(files)) {
   dfs <- Filter(function(x) is.data.frame(get(x)), ls())
 
   # Save RData file
-  save(list = dfs, file = here("outputs", "cea", paste0("CEA_", int, "_", iso, "_", UID, ".RData")))
+  save(list = dfs, file = here("outputs", "cea",  paste0("CEA_", int, "_", iso, "_", UID, ".RData")))
 
   # Remove everything except the file list
   rm(list = setdiff(ls(), "files"))
